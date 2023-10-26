8988dcb33631fe58ba3844281554ac8e7430b875
IsaBicudo <isabella.bicudo.mais.linda@gmail.com>
2023-10-26 08:27:55 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ProjetoEventos.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        
+        }
+    }
+}
